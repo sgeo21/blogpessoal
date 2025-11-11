f15d4d8f00cbef5f14dd464254f34e1fb6e796c8
Geovana <s.geo2112@gmail.com>
2025-11-11 11:47:38 -0300

    :tada: Commit inicial

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import Home from "./pages/home/Home"
+
+
+function App() {
+  
+
+  return (
+    // O fragment<> </> pode ser utilizado pois para o return precisa aglomerar.
+    <>
+    {/* precisa fazer a importação da pagina que aparece ali encima*/}
+      <Home />  
+    </>
+  )
+}
+
+export default App

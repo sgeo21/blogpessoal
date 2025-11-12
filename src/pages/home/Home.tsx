c0a12a54f65953f8b6ef8e92d2fa368ced5ec291
Geovana <s.geo2112@gmail.com>
2025-11-12 16:54:23 -0300

    :sparkles: inclusão tailwindcss

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -5,77 +5,87 @@ function Home() {
   return (
     // Container
     <div
-        style={{
-            backgroundColor:"#312e81",
-            display: "flex",
-            justifyContent: "center"
-        }}
+        // style={{
+        //     backgroundColor:"#312e81",
+        //     display: "flex",
+        //     justifyContent: "center"
+        // }}
+        className="bg-indigo-900 flex justify-center"
     >      
     {/* Grid que divide a rela em duas colunas  */}
         <div
-            style={{
-                display:"grid",
-                gridTemplateColumns: "1fr 1fr",
-                color: "white",
-                width: "100%",
-                maxWidth: "1280px"
-            }}
+            // style={{
+            //     display:"grid",
+            //     gridTemplateColumns: "1fr 1fr",
+            //     color: "white",
+            //     width: "100%",
+            //     maxWidth: "1280px"
+            // }}
+            className="container grid grid-cols-2 text-white"
           > 
            {/* Coluna esquerda */}
             <div
-                style={{
-                    display: "flex",
-                    flexDirection: "column",
-                    gap: "1rem", 
-                    alignItems: "center",
-                    justifyContent: "center",
-                    paddingTop: "1rem",
-                    paddingBottom: "1rem"
-                }}
+                // style={{
+                //     display: "flex",
+                //     flexDirection: "column",
+                //     gap: "1rem", 
+                //     alignItems: "center",
+                //     justifyContent: "center",
+                //     paddingTop: "1rem",
+                //     paddingBottom: "1rem"
+                // }}
+                className="flex flex-col gap-4 items-center justify-center py-4"
              >
                 <h2
-                    style={{
-                        fontSize: "3rem",
-                        fontWeight: "bold"
-                    }}
+                    // style={{
+                    //     fontSize: "3rem",
+                    //     fontWeight: "bold"
+                    // }}
+                    className="text-5xl font-bold"
                  > Seja bem vinde!</h2>
                 <p
-                style={{
-                        fontSize: "1.25rem"
-                    }}
+                // style={{
+                //         fontSize: "1.25rem"
+                //     }}
+                    className="text-xl"
                 > Expresse aqui seus pensamentos e opiniões</p>
                 {/* Link/ botão */}
                 <div
-                    style={{
-                        display:"flex",
-                        justifyContent: "space-around",
-                        gap: "1rem"
-                    }}
-                > 
+                    // style={{
+                    //     display:"flex",
+                    //     justifyContent: "space-around",
+                    //     gap: "1rem"
+                    // }}
+                    className="flex justify-around gap-4"
+                 > 
                     <div
-                        style={{
-                            borderRadius: "0.5rem",
-                            color: "white",
-                            border: "2px solid white", 
-                            padding: "0.5rem 1rem"
-                        }}
-                    >Nova Postagem </div>
+                        // style={{
+                        //     borderRadius: "0.5rem",
+                        //     color: "white",
+                        //     border: "2px solid white", 
+                        //     padding: "0.5rem 1rem"
+                        // }}
+                        className="rounded text-white border-white border-solid border-2 py-2 px-4"
+                     >Nova Postagem 
+                    </div>
                 </div>
             </div>
 
             {/* Coluna direira */}
             <div
-                style={{
-                    display: "flex",
-                    justifyContent: "center"
-                }}
+                // style={{
+                //     display: "flex",
+                //     justifyContent: "center"
+                // }}
+                className="flex justify-center"
              >
                 <img
                     src="https://i.imgur.com/fyfri1v.png"
                     alt="Imagem da Página Home"
-                    style={{
-                        width:"66%"
-                    }}
+                    // style={{
+                    //     width:"66%"
+                    // }}
+                    className="w-2/3"
                 />
             </div>
         
